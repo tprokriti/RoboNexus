6571f63ef0241d0c55442056d810c5a865b422b4
tprokriti <tabia.cse.20200204027@aust.edu>
2023-08-05 23:16:34 +0600

    nationalcomp done

---
+++ national.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>National Page</title>
+</head>
+
+<body>
+    <section class="header">
+        <nav>
+            <a href="index.html"><img src="images/logo.png" /></a>
+            <div class="nav-links">
+                <ul>
+                    <li><a href="index.php">Home</a></li>
+                    <li><a href="aboutus.php">About Us</a></li>
+                    <li><a href="blog.php">Blog</a></li>
+                    <li class="has-submenu">
+                        <a href="#">Competition</a>
+                        <ul class="submenu">
+                            <li><a href="#">Local</a></li>
+                            <li><a href="national.php">National</a></li>
+                            <li><a href="#">International</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="notice.php">Notices</a></li>
+                    <li><a href="#">User</a></li>
+                    <li><a href="#">FAQs</a></li>
+                </ul>
+            </div>
+        </nav>
+        <main>
+            <section class="nation">
+                <h1>Our National Competitions</h1>
+            </section>
+    </section>
+    </main>
+
+    <div class="background">
+        <h2 class="page-title">Our National Competitions</h2>
+        <div class="button-and-rules">
+            <div class="competition-rules">
+                <p>Rules of our national competition</p>
+                <ul>
+
+                    <li> Lorem ipsum dolor sit amet.</li>
+                    <li> Consectetur adipiscing elit.</li>
+                    <li> Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</li>
+                    <li> Consectetur adipiscing elit.</li>
+                    <li> Consectetur adipiscing elit.</li>
+                    <li> Consectetur adipiscing elit.</li>
+                    <li> Consectetur adipiscing elit.</li>
+                    <li> Consectetur adipiscing elit.</li>
+                    <!-- Add more rules as needed -->
+                </ul>
+            </div>
+            <div class="google-form-button">
+                <a href="https://forms.gle/j3pQqcdxMLmK58s6A" target="_blank">Register to the National Competition</a>
+            </div>
+        </div>
+</body>
+
+</html>
