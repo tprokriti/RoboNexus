8dc100b51368da86c2f56cc41955959965ffc39e
tprokriti <tabia.cse.20200204027@aust.edu>
2023-08-05 08:04:00 +0600

    first commit

---
+++ blog.php
@@ -0,0 +1,168 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>Blog - Robotics Club</title>
+  <link rel="stylesheet" type="text/css" href="styles.css" />
+</head>
+
+<body>
+  <section class="header">
+    <nav>
+      <a href="index.html"><img src="images/logo.png" /></a>
+      <div class="nav-links">
+        <ul>
+          <li><a href="index.php">Home</a></li>
+          <li><a href="aboutus.php">About Us</a></li>
+          <li><a href="blog.php">Blog</a></li>
+          <li class="has-submenu">
+            <a href="#">Competition</a>
+            <ul class="submenu">
+              <li><a href="#">Local</a></li>
+              <li><a href="#">Domestic</a></li>
+              <li><a href="#">International</a></li>
+            </ul>
+          </li>
+          <li><a href="notice.php">Notices</a></li>
+          <li><a href="#">User</a></li>
+          <li><a href="#">FAQs</a></li>
+        </ul>
+      </div>
+    </nav>
+    <main>
+      <section class="blog-header">
+        <h1>Blog</h1>
+      </section>
+  </section>
+  </main>
+
+  <!-- About Our Components Section -->
+  <section class="about-components-container">
+    <div class="about-components">
+      <h2>About Our Components</h2>
+      <div class="separator"></div>
+      <div class="image-grid">
+        <div class="component">
+          <a href="https://robu.in/product/mediatek-linkit-one-iot-development-board/">
+            <img src="images/component1.jpg" alt="Component 1" />
+          </a>
+          <div class="component-content">
+            <p>1x Mediatek linkit one</p>
+            <p>LinkIt One is an all-in-one prototyping board for IoT/wearables devices. Integrating GSM, GPRS, Wi-Fi, GPS, Bluetooth features into a basic Arduino form factor. LinkIt ONE is a co-design product by Seeed Studio and MediaTek.</p>
+          </div>
+        </div>
+        <div class="component">
+          <a href="https://www.daraz.com.bd/products/2x-dc-12v-30rpm-gear-motor-high-torque-electric-mini-speed-reduction-geared-motor-eccentric-output-shaft-37mm-gearbox-i242688720-s1186715060.html?search=1">
+            <img src="images/component2.jpg" alt="Component 2" />
+          </a>
+          <div class="component-content">
+            <p>2x Geared motors</p>
+            <p>It is a mechanical system consisting of an electric motor and a gearbox containing a series of gears.</p>
+          </div>
+        </div>
+        <div class="component">
+          <a href="caster_wheel.html">
+            <img src="images/component3.jpg" alt="Component 3" />
+          </a>
+          <div class="component-content">
+            <p>1x Caster wheel</p>
+            <p>A caster is a non-powered wheel that is designed to be fitted to the bottom of a bigger object and used to move it.</p>
+          </div>
+        </div>
+        <div class="component">
+          <a href="micro_servo_motor.html">
+            <img src="images/component4.jpg" alt="Component 4" />
+          </a>
+          <div class="component-content">
+            <p>1x Micro servo motor</p>
+            <p>It is a Servo motor is a type of motor that is powered by a DC source, either from an external supply or by a controller</p>
+          </div>
+        </div>
+        <div class="component">
+          <a href="lithium_battery.html">
+            <img src="images/component5.jpg" alt="Component 5" />
+          </a>
+          <div class="component-content">
+            <p>1x 3.7v Lithium battery</p>
+            <p>The battery is used, the voltage will drop lower and lower until the minimum which is around 3.0V</p>
+          </div>
+        </div>
+        <div class="component">
+          <a href="motor_driver_ic.html">
+            <img src="images/component6.jpg" alt="Component 6" />
+          </a>
+          <div class="component-content">
+            <p>1x L293d motor driver IC</p>
+            <p>L293D IC is a current enhancing IC as the output from the sensor is not able to drive motors itself so L293D is used for this purpose.</p>
+          </div>
+        </div>
+        <div class="component">
+          <a href="arduino_uno.html">
+            <img src="images/component7.jpg" alt="Component 7" />
+          </a>
+          <div class="component-content">
+            <p>Arduino Uno r3</p>
+            <p>It is a microcontroller board based on a removable, dual-inline-package (DIP) ATmega328 AVR microcontroller. </p>
+          </div>
+        </div>
+        <div class="component">
+          <a href="ic_socket.html">
+            <img src="images/component8.jpg" alt="Component 8" />
+          </a>
+          <div class="component-content">
+            <p>1x 16 pin IC socket</p>
+            <p>It is a tool that connects an IC and a printed circuit board electrically to verify the electrical characteristics and to evaluate the reliability of the IC .</p>
+          </div>
+        </div>
+        <div class="component">
+          <a href="jumper_wires.html">
+            <img src="images/component9.jpg" alt="Component 9" />
+          </a>
+          <div class="component-content">
+            <p>Male-female jumper wires</p>
+            <p> Male ends have a pin protruding and can plug into things, while female ends do not and are used to plug things into.</p>
+          </div>
+        </div>
+        <!-- Add more components with anchor tags around the images here -->
+      </div>
+      <!-- Component content (unchanged) -->
+    </div>
+  </section>
+  <!-- Our Modules Section -->
+  <section class="our-modules-container">
+    <div class="our-modules">
+      <h2>Our Modules</h2>
+      <div class="separator"></div>
+      <p>
+        Our robotics club offers various modules to cater to different interests and skill levels. Whether you're a beginner looking to getstarted in robotics or an experienced
+        enthusiast seeking advanced challenges, we have the right module for you. Our expert instructors will guide you through hands-on projects and help you gain valuable
+        knowledge in the field of robotics.
+      </p>
+      <!-- Add your content here for the modules section -->
+    </div>
+  </section>
+  </main>
+
+  <!-- Our Modules Section -->
+  <section class="our-modules-container">
+    <div class="our-modules">
+      <h2>Our Modules</h2>
+      <div class="separator"></div>
+      <p>
+        Our robotics club offers various modules to cater to different interests and skill levels. Whether you're a beginner looking to get started in robotics or an experienced
+        enthusiast seeking advanced challenges, we have the right module for you. Our expert instructors will guide you through hands-on projects and help you gain valuable
+        knowledge in the field of robotics.
+      </p>
+      <!-- Add your content here for the modules section -->
+    </div>
+  </section>
+  <footer id="footer">
+    <div class="footer-content">
+      <p>The website is made by Tabia Morshed</p>
+      <img src="images/author.jpg" alt="Author Image" width="20" height="20" />
+    </div>
+  </footer>
+
+</body>
+
+</html>
---
+++ notice.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>Notices</title>
+  <link rel="stylesheet" type="text/css" href="styles.css" />
+</head>
+
+<body>
+  <section class="header">
+    <nav>
+      <a href="index.html"><img src="images/logo.png" /></a>
+      <div class="nav-links">
+        <ul>
+          <li><a href="index.php">Home</a></li>
+          <li><a href="aboutus.php">About Us</a></li>
+          <li><a href="blog.php">Blog</a></li>
+          <li class="has-submenu">
+            <a href="#">Competition</a>
+            <ul class="submenu">
+              <li><a href="#">Local</a></li>
+              <li><a href="#">Domestic</a></li>
+              <li><a href="#">International</a></li>
+            </ul>
+          </li>
+          <li><a href="notice.php">Notices</a></li>
+          <li><a href="#">User</a></li>
+          <li><a href="#">FAQs</a></li>
+        </ul>
+      </div>
+    </nav>
+    <main>
+      <div class="content-container">
+        <section id="home" class="welcome-section">
+          <h1>Club's Notices</h1>
+          <p>
+            This page contains all the notices that have been posted by the
+            club. You can search for notices by entering the subject of the
+            notice in the search bar.
+          </p>
+        </section>
+        <section id="notices">
+          <h2>Notices</h2>
+          <input type="text" id="search" placeholder="Search for notices..." />
+          <ul id="notice-list">
+            <?php foreach ($notices as $notice) { ?>
+              <li>
+                <a href="#"><?php echo $notice; ?></a>
+              </li>
+            <?php } ?>
+          </ul>
+        </section>
+        <section id="post-notice">
+          <h2>Post a notice</h2>
+          <form action="post_notice.php" method="POST">
+            <input type="text" name="subject" placeholder="Subject" />
+            <textarea name="notice" placeholder="Notice"></textarea>
+            <input type="submit" value="Post" />
+          </form>
+        </section>
+      </div>
+    </main>
+  </section>
+
+  <footer id="footer">
+    <div class="footer-content">
+      <p>The website is made by Tabia Morshed</p>
+      <img src="images/author.jpg" alt="Author Image" width="20" height="20" />
+    </div>
+  </footer>
+</body>
+
+</html>
